5359fe7a9b2348163a7b80b6607c8aafc5a441c1
christiankiller <christian.killer@uzh.ch>
2018-02-28 12:42:39 +0100

    provotum/setup#1: Add network provotum for eth-private-net

--- truffle.js
+++ truffle.js
@@ -6,6 +6,13 @@ module.exports = {
       network_id: "*", // Match any network id,
       gas: 4300000,
       gasPrice: 22000000000
+    },
+    provotum: {
+      host: "localhost",
+      port: 8501,
+      network_id: 15, // Match any network id,
+      gas: 4300000,
+      gasPrice: 22000000000
     }
   }
 };
